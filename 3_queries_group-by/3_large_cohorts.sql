678a3ca73f6e026f1a845de142b1e7279f49c0cd
kmunirpm <km092009@gmail.com>
2021-11-23 02:02:11 +0000

    assignment and assignment submissions - applying GROUP BY and HAVING clauses

---
+++ 3_queries_group-by/3_large_cohorts.sql
@@ -0,0 +1,7 @@
+SELECT cohorts.name as cohort_name, count(*) as student_count 
+FROM cohorts 
+JOIN students ON cohort_id = cohorts.id
+GROUP BY cohorts.name
+HAVING count(*) >= 18
+ORDER BY student_count;
+
---
+++ 3_queries_group-by/4_assignments_total_per_cohorts.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name as cohort, count(*) as total_submissions 
+FROM cohorts 
+JOIN students ON cohort_id = cohorts.id
+JOIN assignment_submissions ON student_id = students.id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
