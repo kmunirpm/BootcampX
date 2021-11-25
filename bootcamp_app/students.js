bed58bc3927bc08cdf01afcaff05faa18e322c28
kmunirpm <km092009@gmail.com>
2021-11-25 05:41:01 +0000

    Bootcamp_app - Protection against SQL Injection implemented

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,12 +6,15 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const cohort_name = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohort_name}%`, limit];
+console.log(values);
 pool.query(`SELECT students.id, students.name, cohorts.name as cohortname, cohort_id 
             FROM students 
             JOIN cohorts ON cohorts.id = cohort_id
-            WHERE cohorts.name LIKE '${process.argv[2]}%' LIMIT ${process.argv[3] || 5};`)
+            WHERE cohorts.name LIKE $1 LIMIT $2;`, values)
 .then(res => {
-  //console.log(res.rows);
   res.rows.forEach(user => {
     console.log(`${user.name} has an id=${user.id} and was in the cohort# ${user.cohort_id} - ${user.cohortname}`);
   })
